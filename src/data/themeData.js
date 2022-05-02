c14c8272949565a763ec4e53bf1ea37b430dde5a
dnyaneshwarmagar <89983890+dnyaneshwarmagar@users.noreply.github.com>
2022-05-02 15:22:39 +0530

    th

--- src/data/themeData.js
+++ src/data/themeData.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import { 
+import {
     greenThemeLight, greenThemeDark, bwThemeLight, bwThemeDark, blueThemeLight, blueThemeDark, redThemeLight, redThemeDark, orangeThemeLight, orangeThemeDark, purpleThemeLight, purpleThemeDark, pinkThemeLight, pinkThemeDark, yellowThemeLight, yellowThemeDark
 } from '../theme/theme'
 
