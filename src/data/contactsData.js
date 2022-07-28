7322ef54fc03c763485fc9bc1426790b7f774834
Dnyaneshwar Magar <89983890+dnyaneshwarmagar@users.noreply.github.com>
2022-07-28 22:21:55 +0530

    Update contactsData.js

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,6 +1,6 @@
 export const contactsData = {
     email: 'dnyaneshwarmagar276@gmail.com',
-    phone: '+917843010355',
+    phone: '+917972895724',
     address: 'Pune, Maharashtra ',
 
     sheetAPI: 'https://sheetdb.io/api/v1/a8sg4ugk7wqqf'
