02097d50ea27b7d78c131f7dbd6dc9cca1c175c6
Dnyaneshwar Magar <89983890+dnyaneshwarmagar@users.noreply.github.com>
2023-10-31 14:29:11 +0530

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,7 +3,7 @@ import img from "../assets/png/img-prof.jpg";
 
 export const headerData = {
     name: 'Dnyaneshwar Magar',
-    title: "Aspiring Full Stack Developer",
+    title: "Full Stack Web Developer",
     desciption: "Love to design, visualize and develope web pages, problem-solver and enjoy collaborating with people! ",
     image: img,
     resumePdf: resume
