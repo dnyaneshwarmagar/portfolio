7ac95c9d54bd99d271db2d7844befb67172f4466
Dnyaneshwar Magar <89983890+dnyaneshwarmagar@users.noreply.github.com>
2023-10-31 14:28:04 +0530

    Update aboutData.js

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "My name's Dnyaneshwar. Learning Full Stack Web Developement Course at Masai.",
-    description2: "Efficient full stack developer with knowledge of Frontend, APIs and Backend technologies. Learned MERN stack. Seeking to further improve web development skills as the future lead  developer.",
+    description1: "My name's Dnyaneshwar. Proficient Full Stack Web Developemer with 1 year of developement experience.",
+    description2: "Efficient full stack developer with knowledge of Frontend, APIs and Backend technologies. Proficient in MERN stack. Seeking to further improve web development skills as the future lead  developer.",
     image: 2
 }
